db0ea8f643f85bbacd5a8c5435461dfa07d5803d
nalcancia <nalcancia@gmail.com>
2024-08-21 23:40:45 -0400

    Added answers to assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -9,7 +9,10 @@ set -x
 
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
 
-mkdir dir1 dir2 dir3 dir4 dir5
+for i in {1..5}
+do
+    mkdir "./dir${i}"
+done
 
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
 
@@ -17,12 +20,27 @@ ls
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
 
+for i in {1..5}
+do
+    touch "./dir2/file${i}"
+done
+
 # 4. Append the words "Hello world" to dir2/file3
+
+echo "Hello world" >> ./dir2/file3
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
 
+cat ./dir2/file3
+
 # 6. Delete file4
+
+rm ./dir2/file4
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 
+rm -rf ./dir[4-5]
+
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
+
+ls
